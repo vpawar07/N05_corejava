ade516a603f464efe87486d777a8cca7397c800d
Vishal <you@example.com>
2023-07-20 12:09:22 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/UserInputDayOne.java
@@ -0,0 +1,39 @@
+// Program to demonstrate on user input
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class UserInputDayOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner s=new Scanner(System.in);
+		
+		/*
+		System.out.println("Enter the value of num");
+		int num=s.nextInt();
+		System.out.println("Enter the value of num is: "+num);
+		
+		System.out.println("Enter the value of floatnum");
+		float num1=s.nextFloat();
+		System.out.println("Enter the value of floatnum is: "+num1);
+		
+		
+		
+		System.out.println("Enter the value of char")
+		char c = s.next().charAt(0);
+		System.out.println("Character is: "+c);
+		*/
+		
+		
+		System.out.println("Enter the value of string");
+		String str1= s.nextLine();
+		//String str1= s.next();
+		System.out.println("String is: "+str1);
+				
+
+	}
+
+}
